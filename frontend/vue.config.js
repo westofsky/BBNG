5458c7a292aba064277df2869cbf9e904c38d4cb
westofsky <westofsky159@gmail.com>
2022-12-28 20:07:03 +0900

    First Commit with MongoDB setting

---
+++ frontend/vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir : path.resolve(__dirname,'../backend/public/'),
+  devServer : {
+    proxy : {
+      '/api':{
+        target : 'https://127.0.0.1:3000/api',
+        changeOrigin : true,
+        pathRewrite : {
+          '^/api': '',
+        },
+      },
+    },
+  },
+});
