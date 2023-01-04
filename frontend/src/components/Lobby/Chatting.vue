0829ba4ab55ff41e49ad71e75977f854e6ff37ac
Sohn Young Jin <sonkim1001@naver.com>
2023-01-04 16:27:22 +0900

    Add chatting delay timer

--- frontend/src/components/Lobby/Chatting.vue
+++ frontend/src/components/Lobby/Chatting.vue
@@ -19,6 +19,8 @@
 
                 ],
                 inputMessage: '',
+                chattingDelayTimer: null,
+                chattingDelayTime: 0,
             }
         },
         methods: {
@@ -27,11 +29,29 @@
                 this.scrollToEnd();
             },
             sendMessage() {
+                if(this.inputMessage.length == 0 || this.chattingDelayTime != 0) return;
+
                 this.messages.push({nickname: 'user', message: this.inputMessage});
                 // Implement send message to server logic.
                 
                 this.inputMessage = "";
                 this.scrollToEnd();
+
+                var inputBox = document.querySelector(".ChattingInput");
+                inputBox.disabled = true;
+                this.chattingDelayTime = 3;
+                inputBox.placeholder = this.chattingDelayTime + "초 후에 전송 가능";
+
+                this.chattingDelayTimer = setInterval(()=> {
+                    this.chattingDelayTime -= 1;
+                    if(this.chattingDelayTime == 0) {
+                        inputBox.disabled = false;
+                        inputBox.placeholder = "메세지를 입력해주세요...";
+                        clearInterval(this.chattingDelayTimer);
+                    } else {
+                        inputBox.placeholder = this.chattingDelayTime + "초 후에 전송 가능";
+                    }
+                }, 1000);
             },
             async scrollToEnd() {
                 await this.$nextTick();
