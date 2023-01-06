71a226f5775a0de1f00e2f9c86f6e776aec7e11d
westofsky <westofsky159@gmail.com>
2023-01-07 02:59:28 +0900

    app.js router추가

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
+app.use('/lobby', indexRouter);
+// api용 
+app.use('/api/users/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
