31d9d50e761f58d02a03fc04b58a254dbc9694dd
le-la <home99032@naver.com>
2023-01-08 19:28:39 +0900

    이미 존재하는 친구신청이나 이미 친구인것 확인 작업

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -5,26 +5,51 @@ var User = require('../Schemas/User');
 var User_t = require('../Schemas/User_t');
 
 router.post('/AddFriend', async (request, response) => {
-    try{
+    try {
         console.log(request.body);
-        const result1 = await User.findOne({user_nickname: request.body.recipient_nickname})
-        const result2 = await User_t.findOne({user_nickname: request.body.recipient_nickname})
+        const result1 = await User.findOne({ user_nickname: request.body.recipient_nickname })
+        const result2 = await User_t.findOne({ user_nickname: request.body.recipient_nickname })
         console.log(result1);
         console.log(result2);
-        if(result1 || result2){
-            Friends.create({
-                user_nickname: request.body.requester_nickname,
-                friend_nickname: request.body.recipient_nickname,
-                status: 0
-            }).then((result) => {
-                response.json({ status:"200", msg : "친구요청에 성공했습니다."});
+        if (result1 || result2) {
+            var flag = true;
+            const result3 = await Friends.find((err, friends) => {
+                friends.forEach((item) => {
+                    if (item.user_nickname == request.body.requester_nickname && item.friend_nickname == request.body.recipient_nickname) {
+                        if (item.status == 0) {
+                            response.json({ status: "500", msg: "이미 친구요청을 보냈습니다." })
+                        }
+                        else if (item.status == 1) {
+                            response.json({ status: "500", msg: "이미 해당 계정과 친구입니다." })
+                        }
+                        flag = false;
+                    }
+                    else if (item.user_nickname == request.body.recipient_nickname && item.friend_nickname == request.body.requester_nickname) {
+                        if (item.status == 0) {
+                            response.json({ status: "500", msg: "상대방이 이미 친구요청을 보냈습니다." })
+                        }
+                        else if (item.status == 1) {
+                            response.json({ status: "500", msg: "이미 해당 계정과 친구입니다." })
+                        }
+                        flag = false;
+                    }
+                })
             })
+            if (flag) {
+                Friends.create({
+                    user_nickname: request.body.requester_nickname,
+                    friend_nickname: request.body.recipient_nickname,
+                    status: 0
+                }).then((result) => {
+                    response.json({ status: "200", msg: "친구요청에 성공했습니다." });
+                })
+            }
         }
-        else{
-            response.json({ status : "500", msg : "존재하지 않는 닉네임입니다."})
+        else {
+            response.json({ status: "500", msg: "존재하지 않는 닉네임입니다." })
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 });
@@ -33,18 +58,18 @@ router.post('/FriendRequest', async (request, response) => {
     var request_list = [];
     const result2 = await Friends.find((err, friends) => {
         friends.forEach((item) => {
-            if(item.friend_nickname == request.body.nickname && item.status == 0){
+            if (item.friend_nickname == request.body.nickname && item.status == 0) {
                 request_list.push(item.user_nickname[0]);
                 console.log("실행됨");
             }
         })
-    }).clone().catch(function(err){ console.log(err)});
+    }).clone().catch(function (err) { console.log(err) });
     console.log(request_list);
-    if(request_list.length == 0){
-        response.json({status:"500"});
+    if (request_list.length == 0) {
+        response.json({ status: "500" });
     }
-    else{
-        response.json({ status:"200", request_list : request_list});
+    else {
+        response.json({ status: "200", request_list: request_list });
     }
 });
 
