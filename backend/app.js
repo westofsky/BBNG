7628c7a8542f4102444946b4ad3beab8b997a5f4
Sohn Young Jin <sonkim1001@naver.com>
2023-01-25 03:22:31 +0900

    주석 및 로그 문구 변경

--- backend/app.js
+++ backend/app.js
@@ -90,8 +90,10 @@ sock_const.initSocketConstants();
 game_const.initGameConstants();
 
 // Set event in io.
-io.on('connection', (socket) => { // New client socket connected.
-  console.log('Client connected: ' + socket.id);
+io.on('connection', (socket) => { // IO Listener Event - 새로운 Client 연결
+  console.log('IO Event(connection): ' + socket.id + ' connected');
+
+  // Socket Listener Event - Client가 연결되었을 때, 접속 중인 Client 리스트에 해당 Client 추가
   socket.on(sock_const.RequestType.ADD_USER_TO_LIST, function (data) {
     clientListBySocket[socket.id] = {
       nickname: data,
@@ -102,43 +104,63 @@ io.on('connection', (socket) => { // New client socket connected.
       rid: '',
     };
     onlineUserList.push(data);
-    console.log(clientListBySocket);
+
+    console.log('Socket Event(ADD_USER_TO_LIST): Connected client list\n########################\n' + JSON.stringify(clientListBySocket) + '\n########################');
   });
-  socket.on(sock_const.RequestType.GET_ONLINE_LIST,() =>{
+
+  // Socket Listener Event - 온라인 상태 친구목록 요청
+  socket.on(sock_const.RequestType.GET_ONLINE_LIST, () => {
     let onlineList = [];
-    for(var i=0;i<Object.values(clientListBySocket).length;i++){
-        onlineList.push(Object.values(clientListBySocket)[i].nickname);
+    for (var i = 0; i < Object.values(clientListBySocket).length; i++) {
+      onlineList.push(Object.values(clientListBySocket)[i].nickname);
     }
     socket.emit(sock_const.ResponseType.RES_ONLINE_LIST, onlineList);
+    console.log("Socket Event(GET_ONLINE_LIST): Client '" + socket.id  + "' request online friend list");
   });
 
+  // Socket Listener Event - Client가 Lobby에 참여
   socket.on(sock_const.RequestType.JOIN_LOBBY, function (data) {
-    console.log("join:" + socket.id);
     socket.join(sock_const.ChatroomType.LOBBY);
+
+    console.log("Socket Event(JOIN_LOBBY): Client '" + socket.id + "' joins the lobby");
   });
 
-  // Leave lobby chatting.
+  // Socket Listener Event - Client가 Lobby에서 떠남
   socket.on(sock_const.RequestType.LEAVE_LOBBY, function (data) {
     socket.leave(sock_const.ChatroomType.LOBBY);
+
+    console.log("Socket Event(LEAVE_LOBBY): Client '" + socket.id + "' leaves the lobby");
   });
 
-  // Receive message from client.
+  // Socket Listener Event - Lobby로 전체 메세지 전달
   socket.on(sock_const.RequestType.SEND_MSG_TO_LOBBY, function (data) {
-    console.log("send message:" + data);
     socket.broadcast.to(sock_const.ChatroomType.LOBBY).emit(sock_const.ResponseType.BROADCAST_LOBBY_MSG, data);
+
+    console.log("Socket Event(SEND_MSG_TO_LOBBY): Player '" + clientListBySocket[socket.id].nickname + "' send message '" + data + "' to lobby");
   });
 
-  // Create custom room.
+  // Socket Listener Event - 사용자 설정 방 생성
   socket.on(sock_const.RequestType.CREATE_ROOM, (gameRoom) => {
     gameRoom.rid = createRoomId((gameRoom.host + (new Date()).toLocaleString()));
+    gameRoom['game_data'] = {
+      ready_count: 0,
+      current_round: 0,
+      current_player: 0,
+      player: [],
+      deck: [],
+      push_deck: [],
+      round_result: [],
+      message_key: '',
+    }
     gameRoomList[gameRoom.rid] = gameRoom;
     socket.join(gameRoom.rid);
     clientListBySocket[socket.id].rid = gameRoom.rid;
     clientListByNickname[clientListBySocket[socket.id].nickname].rid = gameRoom.rid;
-    console.log('Game room created: ' + JSON.stringify(gameRoomList));
+
+    console.log("Socket Event(CREATE_ROOM): Created room list\n########################\n" + JSON.stringify(gameRoomList) + '\n########################');
   });
 
-  // Join custom room.
+  // Socket Listener Event - 사용자 설정 방 참여
   socket.on(sock_const.RequestType.JOIN_ROOM, (data) => {
     console.log(data);
     if (gameRoomList.hasOwnProperty(data.rid)) { // rid 값의 방이 존재할 경우
@@ -155,20 +177,23 @@ io.on('connection', (socket) => { // New client socket connected.
           clientListByNickname[clientListBySocket[socket.id].nickname].rid = data.rid;
 
           socket.emit(sock_const.ResponseType.RES_JOIN_ROOM, sock_const.ResponseResult.RES_JOIN_ROOM_SUCCESS);
+          console.log("Socket Event(JOIN_ROOM): Player '" + data.nickname + "' joins room '" + data.rid + "'");
         } else { // 참여하려는 방의 비밀번호가 일치하지 않을 경우
           socket.emit(sock_const.ResponseType.RES_JOIN_ROOM, sock_const.ResponseResult.RES_JOIN_ROOM_FAILED_WRONG_PASSWORD);
+          console.log("Socket Event(JOIN_ROOM): Player '" + data.nickname + "' failed to join room '" + data.rid + "' => Incorrect password");
         }
       } else { // 방에 빈 자리가 없을 경우
         socket.emit(sock_const.ResponseType.RES_JOIN_ROOM, sock_const.ResponseResult.RES_JOIN_ROOM_FAILED_ROOM_FULL);
+        console.log("Socket Event(JOIN_ROOM): Player '" + data.nickname + "' failed to join room '" + data.rid + "' => Room already full");
       }
     } else {
       // rid 값의 방이 존재하지 않을 경우
       socket.emit(sock_const.ResponseType.RES_JOIN_ROOM, sock_const.ResponseResult.RES_JOIN_ROOM_FAILED_NOT_EXIST);
+      console.log("Socket Event(JOIN_ROOM): Player '" + data.nickname + "' failed to join room '" + data.rid + "' => Room does not exist");
     }
-    console.log(JSON.stringify(gameRoomList));
   });
 
-  // Send room list to client.
+  // Socket Listener Event - 사용자 설정 방 목록 요청
   socket.on(sock_const.RequestType.ROOM_LIST, () => {
     let gameRoomListSendData = {};
     let excludeProps = ['password', 'players'];
@@ -178,40 +203,71 @@ io.on('connection', (socket) => { // New client socket connected.
       gameRoomListSendData[key]['password_required'] = (gameRoomList[key].password != '');
     }
     socket.emit(sock_const.ResponseType.RES_ROOM_LIST, gameRoomListSendData);
+    console.log("Socket Event(ROOM_LIST): Client '" + socket.id + "' request created room list");
   });
 
-  // Client disconnected.
+  // Socket Listener Event - Client 연결 끊김
   socket.on('disconnect', (reason) => {
-    console.log('Client disconnected: ' + socket.id + ' [' + reason + ']');
-    onlineUserList.splice(onlineUserList.indexOf(clientListBySocket[socket.id].nickname),1);
-    if (clientListBySocket[socket.id].rid != '') {
-      gameRoomList[clientListBySocket[socket.id].rid].current_player_count -= 1;
-      if (gameRoomList[clientListBySocket[socket.id].rid].current_player_count == 0) {
-        delete gameRoomList[clientListBySocket[socket.id].rid];
-      } else {
-        gameRoomList[clientListBySocket[socket.id].rid].players = gameRoomList[clientListBySocket[socket.id].rid].players.filter(function (player) {
-          return player.nickname !== clientListBySocket[socket.id].nickname
+    console.log("Socket Event(disconnect): '" + socket.id + "' disconnected => " + reason);
+
+    var socketId = socket.id;
+    var nickname = clientListBySocket[socketId].nickname;
+    var joinedGameRoom = clientListBySocket[socket.id].rid;
+
+    onlineUserList.splice(onlineUserList.indexOf(nickname), 1);
+    if (joinedGameRoom != '') { // 방에 참여한 상태인 경우
+      gameRoomList[joinedGameRoom].current_player_count -= 1;
+      if (gameRoomList[joinedGameRoom].current_player_count == 0) { // 방에 다른 플레이어가 없을 경우
+        console.log("Room Event: Room '" + JSON.stringify(gameRoomList[joinedGameRoom]) + "' removed due to lack of active players");
+        delete gameRoomList[joinedGameRoom];
+      } else { // 방에 다른 플레이어가 남아있을 경우
+        console.log("Room Event: Player '" + nickname + "' has been removed from room '" + gameRoomList[joinedGameRoom] + "'");
+        gameRoomList[joinedGameRoom].players = gameRoomList[joinedGameRoom].players.filter(function (player) {
+          return player.nickname !== nickname
         });
       }
     }
-    delete clientListByNickname[clientListBySocket[socket.id].nickname];
-    delete clientListBySocket[socket.id];
+    delete clientListByNickname[nickname];
+    delete clientListBySocket[socketId];
   });
+
+  // 게임방 관련 socket 처리
 });
 
-// Listening from port.
+// 서버 실행
 httpServer.listen(port, () => {
   console.log(`${port}포트에서 서버 작동중입니다.`);
 });
 
 const crypto = require('crypto');
 
+// Function - 전달받은 RoomName, RoomType과 현재 시간을 기준으로 고유의 RoomId를 만들어줌
 function createRoomId(roomName, roomType) {
   return createHashFromString(roomName + roomType + (new Date()).toLocaleString());
 }
 
+// Function - 문자열을 32 길이의 Hash 문자열로 변환
 function createHashFromString(string) {
   const hash = crypto.createHash('sha256');
   hash.update(string);
   return hash.digest('hex').slice(0, 32);
+}
+
+// Function - 새로운 카드 덱 생성
+function createDeck() {
+  return [
+    'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ',
+    'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ',
+    'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ',
+    'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ',
+  ];
+}
+
+// Function - 전달받은 덱의 카드를 무작위로 섞음
+function shuffleDeck(deck) {
+  for (let loop1 = array.length - 1; loop1 > 0; loop1--) {
+    let loop2 = Math.floor(Math.random() * (loop1 + 1));
+    [deck[loop1], deck[loop2]] = [deck[loop2], deck[loop1]];
+  }
+  return deck;
 }
