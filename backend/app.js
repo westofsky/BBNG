e8200537875a833381c65ab192e86562f6d0b731
Yoon JongBeom <home99032@naver.com>
2023-02-24 04:09:35 +0900

    Hotfix

--- backend/app.js
+++ backend/app.js
@@ -574,13 +574,11 @@ io.on('connection', (socket) => { // IO Listener Event - 새로운 Client 연결
       round: gameRoomList[data.rid].game_data.round_result[gameRoomList[data.rid].game_data.current_round].round,
       player_score: gameRoomList[data.rid].game_data.round_result[gameRoomList[data.rid].game_data.current_round].player_score
     })
-
-    setTimeout(function () { // 최종 라운드 종료 후 2초 뒤에 게임종료.
-      console.log('delay');
-    }, 2000);
-
     gameRoomList[data.rid].game_data.current_round++;
     if(gameRoomList[data.rid].game_data.current_round == gameRoomList[data.rid].round_count){
+      setTimeout(function () { // 최종 라운드 종료 후 2초 뒤에 게임종료.
+        console.log('delay');
+      }, 2000);
       io.to(data.rid).to(data.rid).emit(sock_const.ResponseType.RES_GAME_END);
     }
   });
@@ -619,13 +617,11 @@ io.on('connection', (socket) => { // IO Listener Event - 새로운 Client 연결
       round: gameRoomList[data.rid].game_data.round_result[gameRoomList[data.rid].game_data.current_round].round,
       player_score: gameRoomList[data.rid].game_data.round_result[gameRoomList[data.rid].game_data.current_round].player_score
     })
-
-    setTimeout(function () { // 최종 라운드 종료 후 2초 뒤에 게임종료.
-      console.log('delay');
-    }, 2000);
-
     gameRoomList[data.rid].game_data.current_round++;
     if(gameRoomList[data.rid].game_data.current_round == gameRoomList[data.rid].round_count){
+      setTimeout(function () { // 최종 라운드 종료 후 2초 뒤에 게임종료.
+        console.log('delay');
+      }, 2000);
       io.to(data.rid).to(data.rid).emit(sock_const.ResponseType.RES_GAME_END);
     }
   });
